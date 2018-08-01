8b3de9bfccad3f304607eed8c780656b996f25a0
Riccardo Rolla <riccardo.rolla@gmail.com>
2018-08-01 11:33:46 +0200

    fix cmd file

--- openStudy.cmd
+++ openStudy.cmd
@@ -1,9 +1,10 @@
+@echo off
 if "%1"=="" goto :help
 if "%2"=="" goto :help
 cscript %~dp0\openStudy.js %1 %2
 goto end
 :help
-echo openstudy.cmd [accession Number] [datasource]
+echo openstudy.cmd [datasource] [accession Number] 
 goto end
 
 :end
